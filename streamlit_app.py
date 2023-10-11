ed023b796f117d52aec8edfed534c6b4dcf776c2
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2023-10-11 08:45:14 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,25 @@ import streamlit as st
 from PIL import Image
 from gradio_client import Client
 
+# Nome do arquivo para armazenar o contador de acessos
+contador_arquivo = "contador_acessos.txt"
+
+# Função para ler o valor atual do contador de acessos
+def ler_contador():
+    try:
+        with open(contador_arquivo, "r") as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
+
+# Função para atualizar e salvar o contador de acessos
+def atualizar_contador():
+    total_acessos = ler_contador()
+    total_acessos += 1
+    with open(contador_arquivo, "w") as file:
+        file.write(str(total_acessos))
+    return total_acessos
+
 image = Image.open('cf.png')
 
 # Título da aplicação Streamlit
@@ -13,6 +32,9 @@ question = st.text_input("Digite sua pergunta:")
 
 # Botão para fazer a previsão
 if st.button("Enviar"):
+    # Incrementar e obter o contador de acessos
+    total_acessos = atualizar_contador()
+
     # Criar uma instância do cliente Gradio
     client = Client("FabioSantos/chatbot_cf", hf_token="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")
     
@@ -23,5 +45,9 @@ if st.button("Enviar"):
     st.write("Resposta do Chatbot:")
     st.write(result)
 
+# Exibir o número total de acessos
+st.write(f"Número total de acessos: {ler_contador()}")
+
 # Nota explicativa
 st.markdown("*Desenvolvido por Prof.Dr.Fabio Santos (fssilva@uea.edu.br)*")
+
